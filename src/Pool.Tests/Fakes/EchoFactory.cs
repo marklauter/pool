b5078a5345f46b4a410e2ab390f562c5ba7a41b8
Mark Lauter <mark@marklauter.com>
2025-03-03 20:24:00 -0500

    split factory and prep strat

--- src/Pool.Tests/Fakes/EchoFactory.cs
+++ src/Pool.Tests/Fakes/EchoFactory.cs
@@ -3,16 +3,7 @@ namespace Pool.Tests.Fakes;
 
 internal sealed class EchoFactory
     : IItemFactory<IEcho>
-    , IPreparationStrategy<IEcho>
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("IDisposableAnalyzers.Correctness", "IDISP005:Return type should indicate that the value should be disposed", Justification = "items created by the factory are disposed by the pool or the factory")]
     public IEcho CreateItem() => new Echo();
-
-    public ValueTask<bool> IsReadyAsync(
-        IEcho item,
-        CancellationToken cancellationToken) => ValueTask.FromResult(item.IsConnected);
-
-    public Task PrepareAsync(
-        IEcho item,
-        CancellationToken cancellationToken) => item.ConnectAsync(cancellationToken);
 }
---
+++ src/Pool.Tests/Fakes/EchoPreparationStrategy.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Pool.Tests.Fakes;
+
+internal sealed class EchoPreparationStrategy
+    : IPreparationStrategy<IEcho>
+{
+    public ValueTask<bool> IsReadyAsync(
+        IEcho item,
+        CancellationToken cancellationToken) => ValueTask.FromResult(item.IsConnected);
+
+    public Task PrepareAsync(
+        IEcho item,
+        CancellationToken cancellationToken) => item.ConnectAsync(cancellationToken);
+}
