3e9bfe796af93f13dca0fd1f0b94f89b0e2b6526
Mark Lauter <mark@marklauter.com>
2025-03-16 15:22:49 -0400

    added named pool tests

---
+++ src/Pool.Tests/NamedPoolTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pool.Tests;
+
+public sealed class NamedPoolTests
+{
+    private sealed class PoolItem;
+
+    private sealed class PoolClient(IPool<PoolItem> pool)
+    {
+        public IPool<PoolItem> Pool { get; } = pool ?? throw new ArgumentNullException(nameof(pool));
+    }
+
+    [Fact]
+    public void NamedPool_Registration_Succeeds()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "PoolOptions:UseDefaultFactory", "true" },
+                { "PoolOptions:UseDefaultPreparationStrategy", "true" },
+                { "PoolOptions:PreparationRequired", "true" },
+                { "PoolOptions:MinSize", "1" },
+                { "PoolOptions:MaxSize", "2" },
+                { "PoolOptions:LeaseTimeout", "00:00:00.01" },
+                { "PoolOptions:PreparationTimeout", "00:00:00.01" },
+            }!)
+            .Build();
+
+        using var services = new ServiceCollection()
+            .AddLogging()
+            .AddTransient<PoolItem>()
+            .AddPool<PoolItem, PoolClient>(configuration)
+            .BuildServiceProvider();
+
+        var client = services.GetRequiredService<PoolClient>();
+        Assert.NotNull(client.Pool);
+    }
+}
